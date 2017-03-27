f34e8220bc523672056723b3461eeb9aea712ef2
Yakir Luc Gagnon <12.yakir@gmail.com>
2017-03-27 21:33:07 +0200

    Specify the Gtk signal used to trigger Reactive signal (#11)

--- src/widgets.jl
+++ src/widgets.jl
@@ -341,14 +341,15 @@ textbox(signal::Signal, widget::GtkButtonLeaf, id, preserved = []) =
     Textbox(signal, widget, id, preserved)
 
 """
-    textbox(value=""; widget=nothing, signal=nothing, range=nothing)
-    textbox(T::Type; widget=nothing, signal=nothing, range=nothing)
+    textbox(value=""; widget=nothing, signal=nothing, range=nothing, gtksignal=:activate)
+    textbox(T::Type; widget=nothing, signal=nothing, range=nothing, gtksignal=:activate)
 
 Create a box for entering text. `value` is the starting value; if you
 don't want to provide an initial value, you can constrain the type
 with `T`. Optionally specify the allowed range (e.g., `-10:10`)
 for numeric entries, and/or provide the (Reactive.jl) `signal` coupled
-to this text box.
+to this text box. Finally, you can specify which Gtk signal (e.g. 
+`activate`, `changed`) you'd like the widget to update with.
 """
 function textbox{T}(::Type{T};
                     widget=nothing,
@@ -356,7 +357,8 @@ function textbox{T}(::Type{T};
                     range=nothing,
                     signal=nothing,
                     syncsig=true,
-                    own=nothing)
+                    own=nothing,
+                    gtksignal=:activate)
     if T <: AbstractString && range != nothing
         throw(ArgumentError("You cannot set a range on a string textbox"))
     end
@@ -370,7 +372,7 @@ function textbox{T}(::Type{T};
     end
     setproperty!(widget, :text, value)
 
-    id = signal_connect(widget, :activate) do w
+    id = signal_connect(widget, gtksignal) do w
         push!(signal, entrygetter(w, signal, range))
     end
 
@@ -393,8 +395,9 @@ function textbox{T}(value::T;
                     range=nothing,
                     signal=nothing,
                     syncsig=true,
-                    own=nothing)
-    textbox(T; widget=widget, value=value, range=range, signal=signal, syncsig=syncsig, own=own)
+                    own=nothing,
+                    gtksignal=:activate)
+    textbox(T; widget=widget, value=value, range=range, signal=signal, syncsig=syncsig, own=own, gtksignal=gtksignal)
 end
 
 entrygetter{T<:AbstractString}(w, signal::Signal{T}, ::Void) =
